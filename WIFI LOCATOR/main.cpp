4039f39c840454d243b3fdf21e69e062ef03269d
confiante <eddiemambo@gmail.com>
2015-03-23 22:17:09 -0400

    first commit

---
+++ WIFI LOCATOR/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  WIFI LOCATOR
+//
+//  Created by ISMD on 3/23/15.
+//  Copyright (c) 2015 ISMD. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int zip = 0;
+    
+   cout << "What is your zip code?" << endl;
+   cin >> zip;
+    
+    
+   return 0;
+    
+
+}
