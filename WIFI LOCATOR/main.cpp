d3d1f9090cb7d21dca689eb6c14612c683e8fc5e
confiante <eddiemambo@gmail.com>
2015-03-29 18:39:09 -0400

    update

--- WIFI LOCATOR/main.cpp
+++ WIFI LOCATOR/main.cpp
@@ -23,8 +23,12 @@ int main()
     cout << "What is your name? \n";
     cin >> name;
     
-    cout << "What is your zip code \n";
+    cout << "Enter a zip code within the five boroughs (A ZIP CODE RANGE BETWEEN 10001-11697): " << endl;
     cin >> zip;
+    if (zip < 10000 || zip > 11697){
+        cout << "This is not a zip code within the Five Boroughs...Please try again! \n";
+           return 0;} //Declaring range of New York City 5 Boroughs
+
     
     cout << "What's the farthest miles you'd travel \n";
     cin >> miles;
@@ -34,18 +38,19 @@ int main()
     cout << " Hi! " << User_1.getName() << " The closest Wifi location to " << User_1.getZip() << " is " << User_1.getMiles() * 12 << " miles away at " << endl;
 
     
-    if (User_1.getZip() > 10000 && User_1.getZip() < 10125)
-    {cout << "Thomas Paine Park, New York, NY 10007";}
-    else if (User_1.getZip() > 10250 && User_1.getZip() < 10500)
-    {cout << "Tappen Park Tappen Park, Bay St, Staten Island, NY 10304";}
-    else if (User_1.getZip() > 10750 && User_1.getZip() < 10925)
-    {cout << "Devoe Park 101 W Fordham Rd, Bronx, NY 10468";}
-    else if (User_1.getZip() > 11295 && User_1.getZip() < 10500)
-    {cout << "Prospect Park 95 Prospect Park West, Brooklyn, NY 11215";}
-    else if (User_1.getZip() > 11375 && User_1.getZip() > 10500)
-    {cout << "Flushing Meadows Park Meadow Lake Rd W, Forest Hills, NY 11375";}
+    if (User_1.getZip() > 10001 && User_1.getZip() < 10125){
+       cout << "Thomas Paine Park, New York, NY 10007";}
+    else if (User_1.getZip() > 10250 && User_1.getZip() < 10500){
+       cout << "Tappen Park Tappen Park, Bay St, Staten Island, NY 10304";}
+    else if (User_1.getZip() > 10750 && User_1.getZip() < 10925){
+       cout << "Devoe Park 101 W Fordham Rd, Bronx, NY 10468";}
+    else if (User_1.getZip() > 11295 && User_1.getZip() < 10500){
+       cout << "Prospect Park 95 Prospect Park West, Brooklyn, NY 11215";}
+    else if (User_1.getZip() > 11375 && User_1.getZip() > 10500){
+       cout << "Flushing Meadows Park Meadow Lake Rd W, Forest Hills, NY 11375";}
+    
 
-    return 0;
+return 0;
 
 }
 
